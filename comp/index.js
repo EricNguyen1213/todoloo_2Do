e8fd9fd2b5d13e553d07dc5f7869a87504acab47
EricNguyen1213 <eric.h.nguyen1213@berkeley.edu>
2023-12-22 20:04:02 -0800

    created todos json

--- comp/index.js
+++ comp/index.js
@@ -4,10 +4,34 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const url = require('url');
 const port = 5001;
+var todos = [
+    { itemId: 1,
+        authorId: 1213,
+        name: "Eric Nguyen",
+        title: "Milestone 2",
+        desc: "create that ToDo item",
+        deadline: "2nite",
+        priority: 2 }
+];
 const app = (0, express_1.default)();
-app.get("/hello", (req, res) => {
-    res.status(200).send("Hello User!");
+var reqPath = "";
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+    reqPath = req.path;
+    console.log(reqPath);
+    next();
+});
+app.get("/todos", (req, res) => {
+    console.log(reqPath);
+    res.json(todos);
+});
+app.post("/todos", (req, res) => {
+    const newPost = req.body;
+    todos.push(newPost);
+    res.send("Added 2Do Item");
 });
 app.listen(port, () => {
     console.log(`App Listening on ${port}`);
--- index.ts
+++ index.ts
@@ -1,10 +1,42 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, Request, Response, NextFunction } from "express";
+const url = require('url')
 const port = 5001;
 
-const app: Express = express();
 
-app.get("/hello", (req: Request, res: Response) => {
-    res.status(200).send("Hello User!");
+var todos = [
+    {   itemId: 1, 
+        authorId: 1213, 
+        name: "Eric Nguyen", 
+        title: "Milestone 2", 
+        desc: "create that ToDo item", 
+        deadline: "2nite",
+        priority: 2    }
+];
+
+
+const app: Express = express();
+var reqPath = "";
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+app.use((req, res, next) => {
+    reqPath = req.path;
+    console.log(reqPath)
+    next();
+});
+
+
+app.get("/todos", (req: Request, res: Response) => {
+    console.log(reqPath)
+    res.json(todos);
+});
+
+
+
+app.post("/todos", (req: Request, res: Response) => {
+    const newPost = req.body;
+    todos.push(newPost);
+    res.send("Added 2Do Item");
 });
 
 app.listen(port, () => {
